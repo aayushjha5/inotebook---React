118855e371fe0449a04a0d5948661d843206d658
Aayush Jha <aayushjha5@gmail.com>
2022-03-18 15:07:55 +0530

    init

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const connectToMongo = require('./db');
+const express = require('express')
+
+connectToMongo();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello aayush jha!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
