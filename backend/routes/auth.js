13855b4010ca7c4995934f47ec49cf1fb4f782cc
Aayush Jha <aayushjha5@gmail.com>
2022-03-19 18:36:18 +0530

    creating login endpoint & sending auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -41,11 +41,53 @@ router.post('/createuser', [
       }
     }
     const authToken = jwt.sign(data, JWT_SECRET);
-    res.json({authToken})
+    res.json({ authToken })
   } catch (error) {
     console.log(error.message);
-    res.status(500).send("Some error occured!")
+    res.status(500).send("Internal Server Error")
   };
 })
 
+//CREATING LOGIN ENDPOINT AND SENDING AUTH TOKEN
+
+//Authenticate a User: "/api/auth/login" . No login required
+router.post('/login', [
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+  //if there  are error , return bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {email, password } = req.body;
+  try {
+    //checking whether entered email exists in the DB or not.
+    let user = await User.findOne({email});
+    if (!user){
+      return res.status(400).json({ error: "Invalid email Credentials " });
+    }
+    //compare entered password with user password for the email provided if upper condition is false
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare){
+      return res.status(400).json({ error: "Invalid password Credentials" });
+    }
+
+    //if password is correct then send the payload(data)
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authToken = jwt.sign(data, JWT_SECRET);
+    res.json({authToken})
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(" Internal Server Error");
+  }
+
+
+})
 module.exports = router
